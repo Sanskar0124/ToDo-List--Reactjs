ccb0645a4978c3265ce5f9b3a1e4b828fe71bf19
Sanskar <sansakhraliya@gmail.com>
2021-05-28 12:12:35 +0530

    initial comment

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur enim aperiam qui nulla mollitia suscipit quo, corporis at repellendus dolores vitae odio eaque quaerat non sit quasi. Voluptatibus ratione vel laborum aliquam id officiis.
+            </p>
+        </div>
+    )
+}
